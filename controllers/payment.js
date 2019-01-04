2a4730e0e4d3c3baafed1e4c491d14626a1fccc0
Robert Melendez <robertm3lendez@gmail.com>
2019-01-04 03:29:41 -0500

    add some metadata to save customer to stripe

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,16 +1,27 @@
 const stripe = require('stripe')(process.env.STRIPE_KEY);
+const Cart = require('../models/Cart');
+
 
 exports.chargeCustomer = (req, res, next) => {
     const token = req.body.token.id;
     const email = req.body.email;
-    let amount = req.body.amount;
-    amount = parseInt(amount).toFixed(2) * 100;
 
-
+    const cart = req.body.cart;
+    const customer = req.body.customer;
+    let amount = Math.round(cart.cart.totalPrice * 100);
 
     // Create a new customer and then a new charge for that customer:
     stripe.customers.create({
-        email: email
+        email: email,
+        metadata: {
+            cartId: cart.cart._id,
+            name: customer.name,
+            last_name: customer.last_name,
+            address: customer.address,
+            city: customer.city,
+            zipcode: customer.zipcode
+
+        }
 
     }).then((customer) => {
         return stripe.customers.createSource(customer.id, {
@@ -21,7 +32,11 @@ exports.chargeCustomer = (req, res, next) => {
             amount: amount,
             currency: 'usd',
             customer: source.customer,
-            receipt_email: email
+            receipt_email: email,
+            source: {
+
+            }
+
         });
     }).then((charge) => {
         // New charge created on a new customer
